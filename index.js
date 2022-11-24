9ff82e2b30059bd352ecf6629a0424a1b35e5ab7
nabilAhmedN <nabilahmednahid@gmail.com>
2022-11-25 01:00:14 +0600

    get the category

--- index.js
+++ index.js
@@ -14,6 +14,34 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0byrl94.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+
+async function run() {
+    try{
+        const categoriesCollection = client
+            .db("reGame")
+            .collection("categories");
+
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const options = await categoriesCollection.find(query).toArray();
+            res.send(options)
+        })    
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
 app.get("/", async (req, res) => {
     res.send("reGame portal server is running");
 });
